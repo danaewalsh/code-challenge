e4554dd4f05b9d41689af077baf776937b5d4c8e
Dana Walsh <dana.e.walsh@gmail.com>
2021-06-29 16:19:04 -0700

    Must-have front end build complete : logo, title, username (label + input), password (username + input), button

--- src/pages/create_account.tsx
+++ src/pages/create_account.tsx
@@ -20,7 +20,13 @@ export default function CreateAccount() {
       </Head>
       <article className={styles.article}>
         <form className={styles.form} onSubmit={handleSubmit}>
-          <button>Create Account</button>
+          <img className={styles.logo} src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Wealthfront_Logo.svg/1200px-Wealthfront_Logo.svg.png"/>
+          <h2 className={styles.title}>Create New Account</h2>
+          <label className={styles.inputLabel}>Username</label>
+          <input className={styles.input}/>
+          <label className={styles.inputLabel}>Password</label>
+          <input className={styles.input}/>
+          <button className={styles.submitButton}>Create Account</button>
         </form>
       </article>
     </>
