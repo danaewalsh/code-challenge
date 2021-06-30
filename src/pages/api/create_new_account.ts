4a7659ed7abe854a0d898eda56d29f5ab8ebddc6
Dana Walsh <dana.e.walsh@gmail.com>
2021-06-30 16:04:00 -0700

    Created function to check username against requirements and to check if password has been exposed

--- src/pages/api/create_new_account.ts
+++ src/pages/api/create_new_account.ts
@@ -10,6 +10,42 @@ interface BooleanResult {
   errors?: Record<string, string>;
 }
 
-export default function createNewAccount(req: NextApiRequest, res: NextApiResponse<BooleanResult>) {
+// function to check and see if the password has been exposed
+async function pwExposure(text: string) {
+  const response = await fetch('http://localhost:3000/api/password_exposed', {
+    method: 'POST',
+    body: JSON.stringify({password: text}),
+  });
+  return await response.json();
+}
+
+// check username against requirements
+const properUN = (username: string) => {
+  let reqsMet = new RegExp("^(?=.{10,50}$)");
+  return reqsMet.test(username);
+}
+
+// check password against requirements
+// const properPW = (password: string) {
+
+// }
+
+export default async function createNewAccount(req: NextApiRequest, res: NextApiResponse<BooleanResult>) {
+  // build CreateNewAccountParameters
+  const { username }: CreateNewAccountParameters = JSON.parse(req.body);
+  const { password }: CreateNewAccountParameters = JSON.parse(req.body);
+
+  // check to see if password is exposed
+  const exposed = await pwExposure(password)
+
+  // if password is exposed send response with pwExp error
+
+  // check to see if username fits requirements
+  const checkUN = properUN(username);
+  // if not, send response with unReq error
+
+  // check to see if password fits requirements
+  // if not, send response with pwReq error
+
   res.status(200).json({ result: true });
 }
--- src/pages/create_account.tsx
+++ src/pages/create_account.tsx
@@ -1,15 +1,23 @@
 import Head from 'next/head';
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
 import styles from 'src/styles/create_account.module.scss';
 
 export default function CreateAccount() {
+
+  //
+  const [username, setUN] = useState('');
+  const [password, setPW] = useState('');
+  const [created, setCreated] = useState(false);
+  const [unErr, setUNError] = useState('')
+  const [pwErr, setPWError] = useState('')
+
   async function handleSubmit(evt: FormEvent) {
     evt.preventDefault();
     const response = await fetch('/api/create_new_account', {
       method: 'POST',
-      body: JSON.stringify({}),
+      body: JSON.stringify({ username, password}),
     });
-
+    console.log('submit is working')
     console.log(await response.json());
   }
 
@@ -23,9 +31,15 @@ export default function CreateAccount() {
           <img className={styles.logo} src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Wealthfront_Logo.svg/1200px-Wealthfront_Logo.svg.png"/>
           <h2 className={styles.title}>Create New Account</h2>
           <label className={styles.inputLabel}>Username</label>
-          <input className={styles.input}/>
+          <input
+            className={styles.input}
+            onChange={(e) => {setUN(e.target.value)}}
+          />
           <label className={styles.inputLabel}>Password</label>
-          <input className={styles.input}/>
+          <input
+            className={styles.input}
+            onChange={(e) => {setPW(e.target.value)}}
+          />
           <button className={styles.submitButton}>Create Account</button>
         </form>
       </article>
